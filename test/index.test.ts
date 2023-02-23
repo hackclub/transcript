0e913f2af94226ca2271065ead29ce05cd638a28
Charalampos Fanoulis <yo@fanoulis.dev>
2023-02-24 00:36:43 +0200

    add complete tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,9 +1,34 @@
-import { TranscriptProvider } from '../dist/indexer';
+import { TranscriptProvider } from '../dist/index';
 
 test('can recite a line from transcript', () => {
 	const transcript = new TranscriptProvider('./test/transcript.yml');
 
 	expect(transcript.recite('greeting', {})).toBe('Hello, world!');
+});
+
+test('can recite a nested line from transcript', () => {
+	const transcript = new TranscriptProvider('./test/transcript.yml');
+
+	expect(transcript.recite('errors.notFound', {})).toBe("the dog sniffs around, but doesn't look like it found what it's looking for");
+	expect(transcript.recite('errors.general', {})).toBe('something went wrong!');
+});
+
+test("can return the key when it doesn't exist", () => {
+	const transcript = new TranscriptProvider('./test/transcript.yml');
+
+	expect(transcript.recite('nonexistant', {})).toBe('nonexistant');
+});
+
+test('can use `recite` in string', () => {
+	const transcript = new TranscriptProvider('./test/transcript.yml');
+
+	expect(transcript.recite('welcome', {})).toBe('oh hello! i have tea and a fresh cherry pie cooling off... please come over and have some!');
+});
+
+test('can use variables passed', () => {
+	const transcript = new TranscriptProvider('./test/transcript.yml');
+
+	expect(transcript.recite('name', { name: 'orpheus' })).toBe('nice to meet you, orpheus!');
 });
 
 test('can recite a random line from transcript', () => {
@@ -14,5 +39,4 @@ test('can recite a random line from transcript', () => {
 	const second = transcript.recite('type-of-pie', {});
 	expect(possibles.includes(first)).toBe(true);
 	expect(possibles.includes(second)).toBe(true);
-	expect(first).not.toBe(second);
 });
